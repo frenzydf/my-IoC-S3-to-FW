abae6a0f0e242c82c95aef9e24be6b05e7f11d1a
David Ortega <frenzydf@gmail.com>
2022-11-30 17:46:39 -0500

    Con la segunda parte del codigo

--- s3-update-ioc.py
+++ s3-update-ioc.py
@@ -1,16 +1,21 @@
 #Save new IoC IP address to a List
 
 mylist = []
-with open(r'src/ioc.txt', 'r') as f: #Open your File txt to read and save IoC
-    for line in f:
+with open(r'src/ioc.txt', 'r') as f_actual: #Open your File txt to read and save IoC
+    for line in f_actual:
         mylist.append(line.strip())
 
 #Validation 1 search for a duplicated items
 
-with open(r'cloud/actual.txt', 'r') as fp:
-    content = fp.read()
-for address in mylist:
-    if address in content:
-        print(address ,'already exist')
-    else:
-        print(address ,'is a new item')
+with open(r'cloud/actual.txt', 'r') as f_ioc:
+    for address in mylist:
+        for l_ioc, line_ioc in enumerate(f_ioc):
+            doc_ioc = line_ioc.strip()
+            exist = False
+            if address in doc_ioc:
+                print(address ,'already exist')
+                l_ioc = 0
+                exist = True
+                break
+        if exist == False:
+            print(address ,"is a new item")
