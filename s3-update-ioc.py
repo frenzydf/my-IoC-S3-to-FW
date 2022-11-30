715677096e4eac9d91c8a89c97e228c5da7720fc
David Ortega <frenzydf@gmail.com>
2022-11-30 18:16:24 -0500

    Busqueda entre archivos funcionando

--- s3-update-ioc.py
+++ s3-update-ioc.py
@@ -1,21 +1,9 @@
-#Save new IoC IP address to a List
-
-mylist = []
-with open(r'src/ioc.txt', 'r') as f_actual: #Open your File txt to read and save IoC
-    for line in f_actual:
-        mylist.append(line.strip())
-
-#Validation 1 search for a duplicated items
-
-with open(r'cloud/actual.txt', 'r') as f_ioc:
-    for address in mylist:
-        for l_ioc, line_ioc in enumerate(f_ioc):
-            doc_ioc = line_ioc.strip()
-            exist = False
-            if address in doc_ioc:
-                print(address ,'already exist')
-                l_ioc = 0
-                exist = True
-                break
-        if exist == False:
-            print(address ,"is a new item")
+with open(r'src/ioc.txt', 'r') as f_ioc:
+    for l_ioc, line_ioc in enumerate(f_ioc):
+        my_ioc = line_ioc.strip()
+        with open(r'cloud/actual.txt', 'r') as f_actual:
+            for l_actual, line_actual in enumerate(f_actual):
+                my_actual = line_actual.strip()
+                if my_ioc == my_actual:
+                    print(my_ioc ,"already exist")
+                    break
