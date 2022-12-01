427eac345da95ee7dbb8074c2c5d491276e5596c
David Ortega <frenzydf@gmail.com>
2022-11-30 21:39:54 -0500

    First part: add new item to a IoC list: done

--- s3-update-ioc.py
+++ s3-update-ioc.py
@@ -1,9 +1,23 @@
-with open(r'src/ioc.txt', 'r') as f_ioc:
+def escribir(valor):
+    f = open('cloud/actual.txt', "a")
+    f.write('\n')
+    f.write(valor)
+    print(valor ,"Agregado a IoC")
+    f.close()
+
+
+with open('src/ioc.txt', 'r') as f_ioc:
     for l_ioc, line_ioc in enumerate(f_ioc):
         my_ioc = line_ioc.strip()
-        with open(r'cloud/actual.txt', 'r') as f_actual:
+        exist = False
+        with open('cloud/actual.txt', 'r') as f_actual:
             for l_actual, line_actual in enumerate(f_actual):
                 my_actual = line_actual.strip()
                 if my_ioc == my_actual:
                     print(my_ioc ,"already exist")
+                    exist = True
                     break
+            if exist == False: 
+                escribir(my_ioc)
+    f_actual.close()
+f_ioc.close()
